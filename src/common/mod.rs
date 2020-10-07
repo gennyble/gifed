9a2a3c34ad83ddd88bd2c9233f0c81130707a98d
Brad Alfirevic <brad@genbyte.dev>
2020-10-06 20:05:35 -0500

    Initial commit
    Messy code, but it bloody works! Well, the tests don't fail, which is
    a great start!

---
+++ src/common/mod.rs
@@ -0,0 +1,20 @@
+pub enum Version {
+	Gif87a,
+	Gif89a
+}
+
+pub struct Color {
+	pub r: u8,
+	pub g: u8,
+	pub b: u8
+}
+
+impl Color {
+	pub fn new(r: u8, g: u8, b: u8) -> Self {
+		Self {
+			r,
+			g,
+			b
+		}
+	}
+}
---
+++ src/writer/mod.rs
@@ -0,0 +1,9 @@
+mod outvec;
+mod gifbuilder;
+mod lzw;
+mod imagebuilder;
+
+use outvec::OutVec;
+pub use gifbuilder::GifBuilder;
+use lzw::LZW;
+pub use imagebuilder::ImageBuilder;
