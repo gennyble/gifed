cdedae673268c372beb27c6d2f123cdf21f630f1
Genny <gen@nyble.dev>
2021-03-31 23:56:13 -0500

    Add todos

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,8 @@ pub mod writer;
 pub use color::Color;
 pub use gif::Gif;
 pub use lzw::LZW;
+
+//TODO: Be sure to check that fields in LSD and Img. Desc. that were reserved
+//in 87a aren't set if version is 87a, or that we return a warning, etc. Just
+//remember about this.
+//bottom of page 24 in 89a
