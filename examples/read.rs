545f71709015cdc1a4e9bd63164d5d885da93d7b
Genevive Alfirevic <gen@nyble.dev>
2021-10-12 00:48:58 -0500

    Actually implement image iterator
    I forgot to implement next, oops!

---
+++ examples/read.rs
@@ -0,0 +1,22 @@
+use gifed::{
+	reader::{self, GifReader},
+	writer::ImageBuilder,
+	Gif,
+};
+
+fn main() {
+	let reader = GifReader::file("examples/simulation.gif").unwrap();
+	let first = reader.images().next().unwrap();
+
+	Gif::builder(first.width(), first.height())
+		.palette(first.palette().clone())
+		.image(
+			ImageBuilder::new(first.width(), first.height())
+				.transparent_index(first.transparent_index())
+				.indicies(first.indicies()),
+		)
+		.build()
+		.unwrap()
+		.save("first.gif")
+		.unwrap();
+}
